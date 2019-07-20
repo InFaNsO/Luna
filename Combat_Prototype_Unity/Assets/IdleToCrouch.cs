2c48b176f622669773940cee951d8990b540e238
BhavilG <bhavilg101@gmail.com>
2019-07-20 08:36:24 -0700

    added all the animations started working on the animation tree need scripts to test

---
+++ Combat_Prototype_Unity/Assets/IdleToCrouch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleToCrouch : MonoBehaviour
+{
+    private Animator mAnimator;
+    private float mInputX;
+    private float mInputY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the Animator
+        mAnimator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mInputY = Input.GetAxis("Vertical");
+        mAnimator.SetFloat("InputY", mInputY);
+
+        mInputX = Input.GetAxis("Horizontal");
+        mAnimator.SetFloat("InputX", mInputX);
+    }
+}
---
+++ Combat_Prototype_Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float mSpeed = 4.0f;
+    [SerializeField] private float mRotSpeed = 80.0f;
+    private float mRotation = 0.0f;
+    [SerializeField] private float mGravity = 8.0f;
+
+    private Vector3 mMoveDir = Vector3.zero;
+
+    CharacterController mCharachterController;
+    Animator mAnimationController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mCharachterController = GetComponent<CharacterController>();
+        mAnimationController = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(mCharachterController.isGrounded)
+        {
+            if(Input.GetKey(KeyCode.D))
+            {
+                mMoveDir = new Vector3(1, 0, 0);
+            }
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                mMoveDir = new Vector3(0, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                mMoveDir = new Vector3(-1, 0, 0);
+            }
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                mMoveDir = new Vector3(-1, 0, 0);
+            }
+
+            mMoveDir *= mSpeed;
+        }
+
+        mMoveDir.y -= mGravity * Time.deltaTime;
+        mCharachterController.Move(mMoveDir * Time.deltaTime);
+    }
+}
