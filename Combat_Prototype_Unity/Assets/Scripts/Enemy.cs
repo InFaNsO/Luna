43642cf4bcaf8d5cdeaa4d661d6491092116821b
Peter Zhang <zhangpeter96@hotmail.com>
2019-07-21 03:54:11 -0700

    [Mingzhuo Zhang]--- Add <Enemy.cs>(A EnemyCharator class that has basic function and variables). \n Add <Weapon.cs>(A Weapon class and a bullet class)

---
+++ Combat_Prototype_Unity/Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Charactor : MonoBehaviour     
+{
+    float mCurrentHealth;
+    float mMaxHealth;
+    public abstract void GetHit(float dmg);        
+}                                                   
+
+public class EnemyCharactor : Charactor
+{
+    [SerializeField] Weapon mWeapon;
+
+    public void Attack()
+    {
+
+    }
+
+    public void Die()
+    {
+        //effect
+        Debug.Log("enemy destory");
+
+        // [Maybe] Update game Score
+
+        gameObject.SetActive(false);
+    }
+
+    override public void GetHit(float dmg) 
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider other)
+    {
+        if (other.tag != gameObject.tag)
+        {
+            if (other.GetComponent<Bullet>() != null)
+            {
+                Debug.Log("enemy collide with bullet!");
+
+                GetHit(other.GetComponent<Bullet>().Damage);
+            }
+        }
+    }
+}
