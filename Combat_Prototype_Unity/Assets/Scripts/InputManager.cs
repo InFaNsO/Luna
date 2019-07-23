2eaccccc902a6bac52df035d21393258058f3114
William Chi <williambillchi@hotmail.com>
2019-07-22 21:59:18 -0700

    Created controller and input manager class
    inputmanager key inputs are customizable on unity.

---
+++ Combat_Prototype_Unity/Assets/Scripts/InputManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputManager
+{
+    private static string moveInput = "Move";
+    private static string jumpInput = "Jump";
+    private static string attackInput = "Attack";
+    private static string dodgeInput = "Dodge";
+
+    public static string GetMoveInput()
+    {
+        return moveInput;
+    }
+
+    public static string GetJumpInput()
+    {
+        return jumpInput;
+    }
+
+    public static string GetAttackInput()
+    {
+        return attackInput;
+    }
+
+    public static string GetDodgeInput()
+    {
+        return dodgeInput;
+    }
+
+    public static bool IsButtonPressed(string input)
+    {
+        return Input.GetButton(input);
+    }
+
+}
