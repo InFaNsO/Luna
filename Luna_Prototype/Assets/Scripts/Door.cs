27adc1be1ba51e0e05380d6a7ca0f51f44646fdf
William Chi <williambillchi@hotmail.com>
2019-10-09 18:52:35 -0700

    [William] changed jump to space, added door class.

---
+++ Luna_Prototype/Assets/Scripts/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private bool isDoorLocked;
+
+    public void UnlockDoor()
+    {
+        isDoorLocked = true;
+    }
+
+    public bool GetDoorStatus()
+    {
+        return isDoorLocked;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDoorLocked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
