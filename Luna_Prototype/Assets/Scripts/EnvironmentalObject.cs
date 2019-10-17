e13f6d5c1085333e884cc5c83e1a6deb09564594
ChouChun Nok <424382@AIV.EDMC.STU>
2019-10-16 23:12:04 -0700

    Committer: ChouChun Nok <424382@AIV.EDMC.STU>
     On branch Kevin_KeyDoor_New
     Changes to be committed:
    	new file:   Luna_Prototype/Assets/Prefeb/EnvObj.meta
    	new file:   Luna_Prototype/Assets/Prefeb/EnvObj/Door.prefab
    	new file:   Luna_Prototype/Assets/Prefeb/EnvObj/Door.prefab.meta
    	new file:   Luna_Prototype/Assets/Prefeb/PickUps/Key.prefab
    	new file:   Luna_Prototype/Assets/Prefeb/PickUps/Key.prefab.meta
    	modified:   Luna_Prototype/Assets/Scenes/SampleScene.unity
    	new file:   Luna_Prototype/Assets/Scripts/EnvironmentalObject.cs
    	new file:   Luna_Prototype/Assets/Scripts/EnvironmentalObject.cs.meta
    	new file:   Luna_Prototype/Assets/Scripts/EnvironmentalObjects.meta
    	new file:   Luna_Prototype/Assets/Scripts/EnvironmentalObjects/EnvObj_Door.cs
    	new file:   Luna_Prototype/Assets/Scripts/EnvironmentalObjects/EnvObj_Door.cs.meta
    	new file:   Luna_Prototype/Assets/Scripts/PickUps/Key.cs
    	new file:   Luna_Prototype/Assets/Scripts/PickUps/Key.cs.meta
    	new file:   Luna_Prototype/Assets/Sprites/SpriteKey.png
    	new file:   Luna_Prototype/Assets/Sprites/SpriteKey.png.meta
    	modified:   Luna_Prototype/ProjectSettings/TagManager.asset
    
    	[Added] Key to the Inventory System
    	[Added] EnvironmentalObject Interface
    	[Added] Functional Door that can be unlocked with key in player's inventory

---
+++ Luna_Prototype/Assets/Scripts/EnvironmentalObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnvironmentObjType
+{
+    none,
+    door,
+    lever,
+    fallingplatform
+}
+public interface EnvironmentalObject
+{
+    EnvironmentObjType GetTypeEnum();
+    string GetObjectName();
+    Sprite GetSprite();
+    void interact(ref Player thePlayer);
+}
---
+++ Luna_Prototype/Assets/Scripts/EnvironmentalObjects/EnvObj_Door.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvObj_Door : MonoBehaviour, EnvironmentalObject
+{
+    public Sprite _sprite;
+    public string _name = "door";
+    public bool locked = true;
+    public bool collidePlayer = false;
+    public string GetObjectName()
+    {
+        return _name;
+    }
+
+    public Sprite GetSprite()
+    {
+        return _sprite;
+    }
+
+    public void interact(ref Player thePlayer)
+    {
+        
+    }
+
+    EnvironmentObjType EnvironmentalObject.GetTypeEnum()
+    {
+        return EnvironmentObjType.door;
+    }
+
+    public void Unlock()
+    {
+        if(locked)
+        {
+            locked = false;
+        }
+    }
+
+    public void Update()
+    {
+       if(!locked)
+        {
+            // Door opening logics
+            gameObject.SetActive(false);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            collidePlayer = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collidePlayer = false;
+        }
+    }
+}
