5dd05e429d4a54868636e13572e28fb7b949f5fb
William Chi <williambillchi@hotmail.com>
2019-10-17 14:15:38 -0700

    [william] updated character class with hazard

--- Luna_Prototype/Assets/Scripts/Character.cs
+++ Luna_Prototype/Assets/Scripts/Character.cs
@@ -19,12 +19,33 @@ public abstract class Character : MonoBehaviour
     [SerializeField]
     protected float mJumpStrength;
 
+    //the four variables below are used for hazard damage  - william
+    [SerializeField]
+    private bool mWasTheCharacterInAnHazardInThePastSecond;
+    [SerializeField]
+    private float mReceiveHazardDebuffCD;
+    [SerializeField]
+    private int mReceivedHazardDamage;
+    [SerializeField]
+    private int mReceivedHazardDuration;
+
     public abstract void GetHit(float dmg);
     public abstract void Die();
+
+
+    public void ReceiveDebuff(int debuffDamage, int debuffDuration)
+    {
+        mReceivedHazardDamage = debuffDamage;
+        mReceivedHazardDuration = debuffDuration;
+    }
 
     public void Awake()
     {
         mCurrentHealth = mMaxHealth;
+        mWasTheCharacterInAnHazardInThePastSecond = false;
+        mReceiveHazardDebuffCD = 0.0f;
+        mReceivedHazardDamage = 0;
+        mReceivedHazardDuration = 0;
     }
 
     public void Update()
@@ -33,5 +54,17 @@ public abstract class Character : MonoBehaviour
         {
             Die();
         }
+
+        if(mWasTheCharacterInAnHazardInThePastSecond)
+        {
+            mReceiveHazardDebuffCD += Time.deltaTime;
+        }
+
+        if(mReceiveHazardDebuffCD > 1.0f)
+        {
+            mWasTheCharacterInAnHazardInThePastSecond = false;
+            mReceiveHazardDebuffCD = 0.0f;
+        }
+
     }
 }
