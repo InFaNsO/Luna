f085e25af993a93fd3a07d34ee4a9b2d9143d243
William Chi <williambillchi@hotmail.com>
2019-10-23 18:59:34 -0700

    [William] renamed fire hazard to damage hazard, renamed poison hazard to DOT hazard. moved player animation into player controller

--- Luna_Prototype/Assets/Scripts/DOTHazard.cs
+++ Luna_Prototype/Assets/Scripts/DOTHazard.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireHazard : Hazard
+public class DOTHazard : Hazard
 {
-    public void OnCollisionStay2D(Collision2D collision)
+    [SerializeField]
+    private float hazardDuration;
+
+    private void OnCollisionStay2D(Collision2D collision)
     {
         Character cha = collision.gameObject.GetComponent<Character>();
-        if (cha != null && !cha.GetHazardBool())
-        {
-            cha.GetHit(hazardDamage);
-            cha.SetHazardBool();
-        }
+        if (cha != null)
+            cha.ReceiveDebuff(hazardDamage, hazardDuration);
+            Debug.Log("PoisonDebuff");
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        hazardDamage = 10;
+        hazardDamage = 2;
+        hazardDuration = 10;
     }
 
     // Update is called once per frame
