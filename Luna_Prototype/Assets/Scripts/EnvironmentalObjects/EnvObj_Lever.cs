3351c52bfa8bfc214d845c0f57ce375af1c2d8ae
ChouChun Nok <424382@AIV.EDMC.STU>
2019-10-28 11:10:50 -0700

    [Summary]	Lever initial commit 	[Added]		Lever Prefab 	[Added]		Lever class, a component that allow remote interaction of other environmental objects
    	new file:   Assets/Art/lever0.png
    	new file:   Assets/Art/lever0.png.meta
    	new file:   Assets/Art/lever1.png
    	new file:   Assets/Art/lever1.png.meta
    	new file:   Assets/Prefeb/EnvObj/Lever.prefab
    	new file:   Assets/Prefeb/EnvObj/Lever.prefab.meta
    	modified:   Assets/Scenes/CombatTest.unity
    	modified:   Assets/Scripts/EnvironmentalObjects/EnvObj_Door.cs
    	new file:   Assets/Scripts/EnvironmentalObjects/EnvObj_Lever.cs
    	new file:   Assets/Scripts/EnvironmentalObjects/EnvObj_Lever.cs.meta
    	modified:   ProjectSettings/ProjectVersion.txt

---
+++ Luna_Prototype/Assets/Scripts/EnvironmentalObjects/EnvObj_Lever.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvObj_Lever : MonoBehaviour, EnvironmentalObject
+{
+    private Player plyr;
+    public GameObject interactTarget;
+    private EnvironmentalObject mTarget;
+    public Sprite _spriteOn;
+    public Sprite _spriteOff;
+    private Sprite _currSprite;
+    public string _name = "lever";
+    public bool collidePlayer = false;
+    public bool isTwoWay = false;
+    public leverState flipped = leverState.off;
+
+    SpriteRenderer mSprite;
+
+    public enum leverState
+    {
+        NULL = -1,
+        off,
+        on,
+    }
+    public string GetObjectName()
+    {
+        return _name;
+    }
+
+    public Sprite GetSprite()
+    {
+        return _currSprite;
+    }
+
+    public EnvironmentObjType GetTypeEnum()
+    {
+        return EnvironmentObjType.lever;
+    }
+
+    public void interact(ref Player thePlayer)
+    {
+        switch(flipped)
+        {
+            case leverState.off: //The lever is currently off
+                {
+                    flipped = leverState.on;
+                    mTarget.interact(ref thePlayer);
+                    break;
+                }
+            case leverState.on: //The lever is currently on
+                {
+                    if (isTwoWay)
+                    {
+                        flipped = leverState.off;
+                        mTarget.interact(ref thePlayer);
+                    }
+                    break;
+                }
+            case leverState.NULL:
+                {
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mTarget = interactTarget.GetComponent<EnvironmentalObject>();
+        mSprite = this.GetComponent<SpriteRenderer>();
+        mSprite.sprite = _spriteOff;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Sprite selector, do not touch unless you know what you are doing
+        if(flipped == leverState.on)
+        {
+            mSprite.sprite = _spriteOn;
+        }
+        else if(flipped == leverState.off)
+        {
+            mSprite.sprite = _spriteOff;
+        }
+        else
+        {
+            Debug.Log("Interact Target does not exist");
+        }
+        _currSprite = mSprite.sprite;
+        //end of sprite selector
+
+        if (Input.GetKeyDown(KeyCode.E) && collidePlayer)
+        {
+            interact(ref plyr);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            collidePlayer = true;
+            plyr = other.gameObject.GetComponent<Player>();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collidePlayer = false;
+    }
+}
