3cd572b200e1c1055791c25cead0e31857081561
William Chi <williambillchi@hotmail.com>
2019-11-13 20:26:53 -0800

    [William] created parallax script, created test_background floder in sprites, created parallax test scene.

---
+++ Luna_Prototype/Assets/Scripts/Parallax.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    float imageLength, startPosition;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startPosition = transform.position.x;
+        imageLength = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startPosition + dist, transform.position.y, transform.position.z);
+
+        if (temp > startPosition + imageLength)
+            startPosition += imageLength;
+        else if (temp < startPosition - imageLength)
+            startPosition -= imageLength;
+    }
+}
