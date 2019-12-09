857e3a85cec7e95425840ff5a7c70e14b520fd0f
RolandLilakarsa <1830160@AIV.EDMC.STU>
2019-12-09 12:39:08 -0800

    Added Hazard Mechanics

--- Luna_Prototype/Assets/Scripts/Hazard.cs
+++ Luna_Prototype/Assets/Scripts/Hazard.cs
@@ -8,15 +8,42 @@ public class Hazard : MonoBehaviour
     //protected int hazardDamage;
     //[SerializeField] ElementalType mElement;
     [SerializeField]
+    protected bool delayedTick = false;
+    [SerializeField]
     protected float timePerTick = 1.0f;
     [SerializeField]
     protected bool applyDebuff = false;
+
+    private float mTick;
     private void OnCollisionExit2D(Collision2D collision)
     {
         Character cha = collision.gameObject.GetComponent<Character>();
         if(cha != null)
         {
-            timePerTick = 1.0f;
+            if (delayedTick)
+            {
+                mTick = 0.0f;
+            }
+            else
+            {
+                mTick = timePerTick;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Character cha = collision.gameObject.GetComponent<Character>();
+        if (cha != null)
+        {
+            if (delayedTick)
+            {
+                mTick = 0.0f;
+            }
+            else
+            {
+                mTick = timePerTick;
+            }
         }
     }
 
@@ -26,12 +53,12 @@ public class Hazard : MonoBehaviour
         if (cha != null)
         {
             //cha.ReceiveDebuff(hazardDamage, hazardDuration);
-            if (timePerTick >= 1.0f)
+            if (mTick >= timePerTick)
             {
                 GetComponent<ElementalAttributes>().ApplyDamage(ref cha, applyDebuff);
-                timePerTick = 0.0f;
+                mTick = 0.0f;
             }
-            timePerTick += Time.deltaTime;
+            mTick += Time.deltaTime;
         }
         Debug.Log("HazardDebuff");
     }
@@ -42,19 +69,26 @@ public class Hazard : MonoBehaviour
         if (cha != null)
         {
             //cha.ReceiveDebuff(hazardDamage, hazardDuration);
-            if (timePerTick >= 1.0f)
+            if (mTick >= timePerTick)
             {
                 GetComponent<ElementalAttributes>().ApplyDamage(ref cha, applyDebuff);
-                timePerTick = 0.0f;
+                mTick = 0.0f;
             }
-            timePerTick += Time.deltaTime;
+            mTick += Time.deltaTime;
         }
         Debug.Log("HazardDebuff");
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(delayedTick)
+        {
+            mTick = 0.0f;
+        }
+        else
+        {
+            mTick = timePerTick;
+        }
     }
 
     // Update is called once per frame
