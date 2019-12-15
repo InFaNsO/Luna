7132939c22f376d8ebebeb10c395040f72e3e9ad
BhavilG <bhavilg101@gmail.com>
2019-12-15 12:59:55 -0800

    [Bhavil] aded big bois

--- Luna_Prototype/Assets/Scripts/AI/Enemies/EnemyBigBois.cs
+++ Luna_Prototype/Assets/Scripts/AI/Enemies/EnemyBigBois.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBigBois : MonoBehaviour
+public class EnemyBigBois : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    LAI.States mCurrentState = LAI.States.None;
+    [SerializeField] LAI.WanderGroundState stateWander = new LAI.WanderGroundState();
+
+    private new void Awake()
     {
-        
+        base.Awake();
+        mSteeringModule.AddState<LAI.BehaviourSeek>();
+        mSteeringModule.SetActive(LAI.SteeringType.Seek, true);
+    }
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        mStateMachine.SetAgent(this);
+        mStateMachine.AddState<LAI.AttackState>();
+        mStateMachine.AddState<LAI.SupriseState>();
+        mStateMachine.AddState<LAI.GoToPlayerState>();
+        mStateMachine.AddState<LAI.WanderGroundState>(stateWander);
+        mCurrentState = LAI.States.Wander;
+        mStateMachine.ChangeState((int)mCurrentState);
     }
 
     // Update is called once per frame
-    void Update()
+    new void Update()
     {
-        
+        base.Update();
+        //state machine update will handle switching state
     }
 }
