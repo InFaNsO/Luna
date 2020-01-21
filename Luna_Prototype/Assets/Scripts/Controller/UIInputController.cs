c11084a797a8f664a4a9982e11eb457cea54a833
William Chi <williambillchi@hotmail.com>
2020-01-20 19:47:06 -0800

    [William] updated input controller for the UI control tab, added UI input Controller script, TO FIX: inputs under ui controller are not being recognized

---
+++ Luna_Prototype/Assets/Scripts/Controller/UIInputController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UIInputController : MonoBehaviour
+{
+    InputController controls;
+
+    private void Awake()
+    {
+        controls = new InputController();
+        controls.UIControl.GameStart.performed += _start => GameStart();
+    }
+
+    public void GameStart()
+    {
+        Debug.Log("game start");
+        ServiceLocator.Get<GameManager>().SwitchScene(GameManager.ESceneIndex.Level1);
+    }
+}
