52712bfe3b7bd8fda73cab8552a73567f2eb9caa
Mingzhuo <zhangpeter96@hotmail.com>
2020-01-20 22:16:57 -0800

    [Mingzhuo Zhang]--- Testing the double jump condition with unity physics

--- Luna_Prototype/Assets/Scripts/Controller/PlayerAnimatorController.cs
+++ Luna_Prototype/Assets/Scripts/Controller/PlayerAnimatorController.cs
@@ -12,11 +12,16 @@ public class PlayerAnimatorController : MonoBehaviour
 
     PlayerController mPlayerController;
     Animator mAnimator;
+    Rigidbody2D rb;
+    float mLastYVel = 0.0f;
+    float mLastAccelration = 0.0f;
+
     PlayerAnimationState mCurrentState = PlayerAnimationState.PLAYER_IDLE;
     void Awake()
     {
         mPlayerController = GetComponent<PlayerController>();
         mAnimator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -24,6 +29,22 @@ public class PlayerAnimatorController : MonoBehaviour
         mCurrentState = mPlayerController.IsMoving() ? PlayerAnimationState.PLAYER_RUN : PlayerAnimationState.PLAYER_IDLE;
 
         mAnimator.SetInteger("playerState", (int)mCurrentState);
+
+       
+    }
+
+    private void FixedUpdate()
+    {
+        float currentVelY = rb.velocity.y;
+        float currentAccelation = (currentVelY - mLastYVel);
+        Core.Debug.Log("" + currentAccelation);
+        if (currentAccelation > mLastAccelration && currentVelY > 0.0f)
+        {
+            Core.Debug.Log(" Jump !!!!!!!!!!!!!!!!!");
+            mAnimator.SetBool("IsDoubleJump", true);
+        }
+        mLastYVel = currentVelY;
+        mLastAccelration = currentAccelation;
     }
 
     void LateUpdate()
@@ -32,6 +53,7 @@ public class PlayerAnimatorController : MonoBehaviour
         {
             mAnimator.SetInteger("playerState", (int)PlayerAnimationState.PLAYER_IDLE);
             mAnimator.SetBool("IsOnGround", mPlayerController.IsGrounded());
+            mAnimator.SetBool("IsDoubleJump", false);
         }
     }
 }
