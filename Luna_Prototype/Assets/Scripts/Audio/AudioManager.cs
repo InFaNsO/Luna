781214dcbb9ce7b16fc1c205e710f7de9dd41722
HuangRuxin <1740008@AIV.EDMC.STU>
2020-01-25 16:01:05 -0800

    [RH] add audiomanager, mod gameloader to grab it from [Gameloader] scene

---
+++ Luna_Prototype/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer _audioMixer;
+ 
+     private void Awake()
+    {
+        Debug.Log("[AudioManager] Awake");
+        DontDestroyOnLoad(gameObject);
+    }
+      public void SetMasterVolume(float vol)
+    {
+        _audioMixer.SetFloat("Master_Volume", vol);
+    }
+    public   void SetMusicVolume(float vol)
+    {
+        _audioMixer.SetFloat("Music_Volume", vol);
+
+    }
+    public   void SetSFXVolume(float vol)
+    {
+        _audioMixer.SetFloat("SFX_Volume", vol);
+
+    }
+
+
+}
