a9dabc2e906a973d6acb9d25a3b144112a8af777
claire <keisuke.sato@lasallecollegevancouver.com>
2020-02-24 19:42:07 -0800

    The play button works, so title scene should be mostly done.

---
+++ Luna_Prototype/Assets/Experiments/Scenes/Claire_Scenes/Scene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  
+
+public class Scene : MonoBehaviour
+{
+
+    public void Play()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void Quit()
+    {
+        //Code to quit game here
+    }
+
+    public void Credits()
+    {
+        //SceneManager.LoadScene("CreditsScene");
+        //There is no credits scene yet
+    }
+}
