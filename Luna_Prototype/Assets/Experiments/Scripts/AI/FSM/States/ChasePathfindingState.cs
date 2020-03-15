ce9dff9b78262794cddf77e21063d207c1b89bf2
RolandLilakarsa <1830160@AIV.EDMC.STU>
2020-03-15 13:24:17 -0700

    Level Arts update

--- Luna_Prototype/Assets/Experiments/Scripts/AI/FSM/States/ChasePathfindingState.cs
+++ Luna_Prototype/Assets/Experiments/Scripts/AI/FSM/States/ChasePathfindingState.cs
@@ -168,33 +168,33 @@ public class ChasePathfindingState : State
             Gizmos.DrawLine(mAgent.transform.position, mAgent.mZone.mPlayerTransform.position);
     }
 
-    public override void DebugDraw()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(mAgent.transform.position, mAgent.transform.position + new Vector3(mAgent.mRigidBody.velocity.x * 10, mAgent.mRigidBody.velocity.y * 10, 0.0f));
-
-        Gizmos.DrawWireSphere(mAgent.mAgent.mTarget, 0.5f);
-
-        Gizmos.color = Color.green;
-
-        if (mAgent.mAgent.mPath.Count == 0)
-            return;
-
-        Gizmos.DrawWireSphere(mAgent.mAgent.mPath[mAgent.mAgent.mPath.Count - 1], 0.5f);
-
-        for (int i = 0; i < finder.mNodes.Count; ++i)
-        {
-            Gizmos.color = Color.magenta;
-            Gizmos.DrawWireSphere(finder.mNodes[i].pos, 0.3f);
-
-            Gizmos.color = Color.cyan;
-
-            for (int j = 0; j < finder.mNodes[i].childrenID.Count; ++j)
-            {
-                Gizmos.DrawLine(finder.mNodes[i].pos, finder.mNodes[finder.mNodes[i].childrenID[j]].pos);
-            }
-        }
-    }
+    //public override void DebugDraw()
+    //{
+    //    Gizmos.color = Color.yellow;
+    //    Gizmos.DrawLine(mAgent.transform.position, mAgent.transform.position + new Vector3(mAgent.mRigidBody.velocity.x * 10, mAgent.mRigidBody.velocity.y * 10, 0.0f));
+    //
+    //    Gizmos.DrawWireSphere(mAgent.mAgent.mTarget, 0.5f);
+    //
+    //    Gizmos.color = Color.green;
+    //
+    //    if (mAgent.mAgent.mPath.Count == 0)
+    //        return;
+    //
+    //    Gizmos.DrawWireSphere(mAgent.mAgent.mPath[mAgent.mAgent.mPath.Count - 1], 0.5f);
+    //
+    //    for (int i = 0; i < finder.mNodes.Count; ++i)
+    //    {
+    //        Gizmos.color = Color.magenta;
+    //        Gizmos.DrawWireSphere(finder.mNodes[i].pos, 0.3f);
+    //
+    //        Gizmos.color = Color.cyan;
+    //
+    //        for (int j = 0; j < finder.mNodes[i].childrenID.Count; ++j)
+    //        {
+    //            Gizmos.DrawLine(finder.mNodes[i].pos, finder.mNodes[finder.mNodes[i].childrenID[j]].pos);
+    //        }
+    //    }
+    //}
 
     public override void Exit()
     {
