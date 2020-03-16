8d84132612cb63d65f79d2eae3df9906c50b6ae0
RolandLilakarsa <1830160@AIV.EDMC.STU>
2020-03-16 12:00:43 -0700

    [Mingzhuo Zhang]--- Hook the scene

---
+++ Luna_Prototype/Assets/Scripts/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+
+    public void PlayAgain()
+    {
+        ServiceLocator.Get<GameManager>().SwitchScene(GameManager.ESceneIndex.Level1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
