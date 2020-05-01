85329b4f3d15b849f8501bc767996cfbc0454fcb
Bhavil Gupta <40411844+InFaNsO@users.noreply.github.com>
2020-05-02 03:40:59 +0530

    Upgraded unity to version 2019.3.6f1 and added LWRP

---
+++ Assets/Shaders/DissolveEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveEffect : MonoBehaviour
+{
+    [SerializeField] Material DissolveMaterial;
+    public float DissolveAmount = 1.0f;
+
+    private void Start()
+    {
+        DissolveMaterial.SetFloat("_DissolveAmount_Dissolve", DissolveAmount);
+    }
+
+    private void Update()
+    {
+        DissolveMaterial.SetFloat("_DissolveAmount_Dissolve", DissolveAmount);
+    }
+
+    public void IncreaseDissolve(float val = 0.0f)
+    {
+        if (val == 0f)
+            val = Time.deltaTime;
+
+        DissolveAmount -= val;
+        DissolveAmount = Mathf.Clamp01(DissolveAmount);
+        DissolveMaterial.SetFloat("_DissolveAmount_Dissolve", DissolveAmount);
+    }
+}
