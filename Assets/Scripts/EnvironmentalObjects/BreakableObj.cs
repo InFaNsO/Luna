68933b659baf500282d3596f0a73d9eb1576aa9e
Roland <rlilakarsa@gmail.com>
2020-05-22 00:35:30 -0700

    Fixing sprites collider and adjust level

---
+++ Assets/Scripts/EnvironmentalObjects/BreakableObj.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableObj : Character
+{
+    SpriteRenderer spriteRenderer;
+    public Sprite notBroken;
+    public Sprite broken;
+    public Vector3 particlePosOffset;
+    public ParticleSystem particleOnBroken;
+    //public AudioClip mAudioSource;
+    //public AudioClip audioOnHit;
+    //public AudioClip audioOnBroken;
+    bool isbroken = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myHealth = GetComponent<Health>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = notBroken;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag != gameObject.tag)
+        {
+            var bullet = other.GetComponent<Bullet>();
+            if (bullet != null)
+            {
+                mLastGotHitPosition = other.gameObject.transform.position;
+                GetHit(bullet.Damage, mLastGotHitPosition);
+            }
+
+            if (myHealth.GetHealth() > 0)
+            {
+                spriteRenderer.sprite = notBroken;
+            }
+            else
+            {
+                spriteRenderer.sprite = broken;
+            }
+        }
+
+        if(!isbroken && myHealth.GetHealth() <= 0)
+        {
+            if (particleOnBroken != null)
+            {
+                Instantiate(particleOnBroken, transform.position + particlePosOffset, Quaternion.identity);
+            }
+            isbroken = true;
+        }
+    }
+}
