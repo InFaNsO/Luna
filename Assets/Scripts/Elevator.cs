837f756d1c7e4a7cd4c277c2d1faf3cb22338998
xrhrepus <xrhrepus@gmail.com>
2020-05-29 09:48:45 -0700

    [RH],add,add SFX to Elevator 1,2 prefab and elevator.cs

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Elevator : MonoBehaviour
-{ 
+{
+    [SerializeField]
+    private SFXGroup _SFXGroup;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             GetComponent<Animator>().SetBool("Trigger1", true);
+            _SFXGroup.PlaySFX("Elevator_Up");
+
         }
     }
 }
